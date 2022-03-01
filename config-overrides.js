fc2d6204def62760ae2a6f24da05893148718271
Alexey Shirokov <shirokovam@lad24.ru>
2022-03-01 14:55:04 +0300

    some fix 1

--- config-overrides.js
+++ config-overrides.js
@@ -8,7 +8,7 @@ module.exports = function override(config, env) {
 
   config.output = {
     ...config.output,
-    publicPath: "http://localhost:3001/",
+    publicPath: "auto",
     clean: true,
     crossOriginLoading: "anonymous",
   };
